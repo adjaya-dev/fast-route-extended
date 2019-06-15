e17ca700d08568b3d6a9b006e301eb1d85b90f2e
adjaya-dev <adjaya-dev@orange.fr>
2019-06-15 20:39:03 +0200

    Factorisation

--- src/Addon/RouteCollectorDecoratorAddonInterface.php
+++ src/Addon/RouteCollectorDecoratorAddonInterface.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Adjaya\FastRoute\Addon;
 
-use Adjaya\FastRoute\Handling\HandlingInterface;
 use Adjaya\FastRoute\RouteCollectorDecoratorInterface;
 
 interface RouteCollectorDecoratorAddonInterface extends RouteCollectorDecoratorInterface {}
--- src/Configurator.php
+++ src/Configurator.php
@@ -6,5 +6,22 @@ namespace Adjaya\FastRoute;
 
 class Configurator implements ConfiguratorInterface
 {
+    protected $options;
 
+    protected $class;
+
+    public function getOptions(): array
+    {
+        return $this->options;
+    }
+
+    public function getClass(): string
+    {
+        return $this->class;
+    }
+
+    public function provide(): array
+    {
+        return [$this->getClass, $this->getOptions];
+    }
 }
